52d9d2e2d7bb00c93bfe82b813caa15dfc8c314b
IrvinFrias <irvin.frias.unam@outlook.com>
2023-03-02 11:36:52 -0600

    app router done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import {RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './Components/home/home.component';
+import {HomeComponent} from "./Components/home/home.component";
 import { CarouselComponent } from './Components/carousel/carousel.component';
 import { ChooseUsComponent } from './Components/choose-us/choose-us.component';
 import { AboutShopComponent } from './Components/about-shop/about-shop.component';
 import { OurProductsComponent } from './Components/our-products/our-products.component';
 import { OurGalleryComponent } from './Components/our-gallery/our-gallery.component';
-import {NgOptimizedImage} from "@angular/common";
 import { CustomerSaysComponent } from './Components/customer-says/customer-says.component';
 import { NewslatterComponent } from './Components/newslatter/newslatter.component';
 import { FooterComponent } from './Components/footer/footer.component';
@@ -32,9 +31,15 @@ import { FooterComponent } from './Components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{path: 'home', component: HomeComponent}]),
+    RouterModule.forRoot(
+      [
+        {path: '', component: HomeComponent},
+        {path: 'about', component:AboutShopComponent},
+        {path: 'products', component:OurProductsComponent},
+        {path: 'contact', component:OurGalleryComponent}
+      ]
+    ),
     NgbModule,
-    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
